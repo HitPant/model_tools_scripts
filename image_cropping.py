bf2f8ef4094dbbec100b0329e8b3b838a021778f
HitPant <hitesh.pant21@gmail.com>
2021-04-24 13:42:58 +0530

    tools

--- image_cropping.py
+++ image_cropping.py
@@ -4,12 +4,16 @@ import os
 
 
 
-dir = "/home/foss/Documents/Deep_learning_prep/gesture_dataset"
+dir = input("destination folder: ")
+save_folder = input("save_path: ")
 
-count = 67
+height= int(input("height: ")) 
+width = int(input("width: "))
+
+count = 0
 
 for img in sorted(os.listdir(dir)):
-    if img.endswith(".jpg"):
+    if img.endswith(".jpg") or img.endswith(".png") or img.endswith(".jpeg"):
         image = cv2.imread(os.path.join(dir,img))
         
 
@@ -17,8 +21,8 @@ for img in sorted(os.listdir(dir)):
 
         crop_img = image[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 
-        resized_img = cv2.resize(crop_img, (300,300))
-        cv2.imwrite(f"/home/foss/Music/img{count}.jpg", resized_img)
+        resized_img = cv2.resize(crop_img, (height, width))
+        cv2.imwrite(os.path.join(save_folder, str(count)+".jpg"), resized_img)
         count+=1
 
 
